43c1e9c55b95c4957a3223f331ca55934bcb9880
Jason Kreidler <jason.kreidler@gmail.com>
2024-04-11 18:36:12 -0500

    Matts Climb B

--- src/main/java/frc/robot/commands/Secondary/LowerCmd.java
+++ src/main/java/frc/robot/commands/Secondary/LowerCmd.java
@@ -11,7 +11,9 @@ public class LowerCmd extends Command {
 
   private final ClimberSubsystem m_climberSubsystem;
   private boolean lowered = false;
-  private double lowerDist = 13.0;
+  private boolean loweredL = false;
+  private boolean loweredR = false;
+  //private double lowerDist = 13.0;
 
   public LowerCmd(ClimberSubsystem climberSubsystem) {
     this.m_climberSubsystem = climberSubsystem;
@@ -23,28 +25,47 @@ public class LowerCmd extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+  
     lowered = false;
+    loweredR = false;
+    loweredL = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    if (Math.abs(m_climberSubsystem.climberEncoderR.getPosition()) <= lowerDist){
-      m_climberSubsystem.climberMotorR.set(-.75);
-    } else if (Math.abs(m_climberSubsystem.climberEncoderR.getPosition()) >= lowerDist){
-      m_climberSubsystem.climberMotorR.set(0);
-    }
+  public void execute(){
 
-    if (Math.abs(m_climberSubsystem.climberEncoderL.getPosition()) <= lowerDist){
+  // if climber position is LESS THAN 12        
+  // then set climber speed to -.75
+
+      //else if climber position is LESS THAN 13
+      //then set climber speed to -.25
+          
+         // else set climber speed to 0
+      
+      if (Math.abs(m_climberSubsystem.climberEncoderR.getPosition()) < 10) {
+        m_climberSubsystem.climberMotorR.set(-.75);
+      } else if (Math.abs(m_climberSubsystem.climberEncoderR.getPosition()) < 13){
+        m_climberSubsystem.climberMotorR.set(-.25);
+      } else {
+        m_climberSubsystem.climberMotorR.set(0);
+        loweredR = true;
+      }
+    if (Math.abs(m_climberSubsystem.climberEncoderL.getPosition()) < 10) {
       m_climberSubsystem.climberMotorL.set(-.75);
-    } else if (Math.abs(m_climberSubsystem.climberEncoderL.getPosition()) >= lowerDist){
+    } else if (Math.abs(m_climberSubsystem.climberEncoderL.getPosition()) < 13) {
+      m_climberSubsystem.climberMotorL.set(-.25);
+    } else {
       m_climberSubsystem.climberMotorL.set(0);
+      loweredL = true;
     }
-  
-    if (Math.abs(m_climberSubsystem.climberEncoderR.getPosition()) >= lowerDist && Math.abs(m_climberSubsystem.climberEncoderL.getPosition()) >= lowerDist){
-    lowered = true;
-    }
+    
+        // if both climber arms are lowered
+        // then set lowered to true
 
+    if (loweredR == true && loweredL == true) {
+      lowered = true;
+    }
   }
 
   // Called once the command ends or is interrupted.
@@ -52,11 +73,15 @@ public class LowerCmd extends Command {
   public void end(boolean interrupted) {
     m_climberSubsystem.climberMotorL.set(0);
     m_climberSubsystem.climberMotorR.set(0);
+    if(interrupted){
+      m_climberSubsystem.climberMotorL.set(0);
+      m_climberSubsystem.climberMotorR.set(0);
+    }
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return lowered;
+    return lowered;    
   }
 }
